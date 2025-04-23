45151a92bfcb91e2245ed2776c16ee43370a4307
Sohaib Rafiq <sohaibrafiq991@gmail.com>
2025-04-24 00:07:13 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/boolean', (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
+
+app.post('/api/check-age', (req, res) => {
+    const age = req.body.age;
+    if (age >= 18) {
+      res.json({ allowed: true });
+    } else {
+      res.json({ allowed: false });
+    }
+  });
+
+  app.post('/api/verify', (req, res) => {
+    const { value } = req.body;
+  
+     if (value === 'yes') {
+      res.json({ boolean: true, number: 1 });
+    } else {
+      res.json({ boolean: false, number: 0 });
+    }
+  });
+
+  app.get('/api/verify', (req, res) => {
+    const value = req.query.value;
+  
+    if (value === '1') {
+      res.send(true);  
+    } else {
+      res.send(false); 
+    }
+  });
